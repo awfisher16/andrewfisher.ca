69ab00ac35318bdb095af8edde19db9bba151baa
Andrew Fisher <awfisher16@gmail.com>
2015-06-02 23:53:31 -0400

    Added Javascript for bullets and industry experience.

--- source/js/all.js
+++ source/js/all.js
@@ -1,5 +1,45 @@
-//Strucutral Engineering Modal
-$(function() {
+//Strucutral Engineering Collapse
+$(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger-1");
+  var expanderContent = document.getElementById("js-expander-content-1");
+
+  $("#js-expander-trigger-1").click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
+
+//Project Management Collapse
+$(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger-2");
+  var expanderContent = document.getElementById("js-expander-content-2");
+
+  $("#js-expander-trigger-2").click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
+
+//Technical Research Collapse
+$(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger-3");
+  var expanderContent = document.getElementById("js-expander-content-3");
+
+  $("#js-expander-trigger-3").click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
+
+//Regulation & Public Policy Collapse
+$(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger-4");
+  var expanderContent = document.getElementById("js-expander-content-4");
+
+  $("#js-expander-trigger-4").click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
+
+//Structural Engineering Modal
+$(document).ready(function() {
   $("#modal-1").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
@@ -18,7 +58,7 @@ $(function() {
 });
 
 //Bridge Rehabilitation Modal
-$(function() {
+$(document).ready(function() {
   $("#modal-2").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
@@ -31,13 +71,13 @@ $(function() {
     $(".modal-state:checked").prop("checked", false).change();
   });
 
-  $(".modal-inner-2").on("click", function(e) {
+  $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
 });
 
 //Wind Engineering Modal
-$(function() {
+$(document).ready(function() {
   $("#modal-3").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
@@ -50,13 +90,13 @@ $(function() {
     $(".modal-state:checked").prop("checked", false).change();
   });
 
-  $(".modal-inner-3").on("click", function(e) {
+  $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
 });
 
 //Land Development Modal
-$(function() {
+$(document).ready(function() {
   $("#modal-4").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
@@ -69,13 +109,13 @@ $(function() {
     $(".modal-state:checked").prop("checked", false).change();
   });
 
-  $(".modal-inner-4").on("click", function(e) {
+  $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
 });
 
 //Concrete Inspection and Testing Modal
-$(function() {
+$(document).ready(function() {
   $("#modal-5").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
@@ -88,8 +128,7 @@ $(function() {
     $(".modal-state:checked").prop("checked", false).change();
   });
 
-  $(".modal-inner-5").on("click", function(e) {
+  $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
 });
-
